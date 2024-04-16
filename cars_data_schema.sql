0b7a6d7a28ef521bc87657186a7357825d335c86
Amer Banaweer <amerbnr10@gmail.com>
2024-04-17 02:54:28 +0300

    editing code

--- cars_data_schema.sql
+++ cars_data_schema.sql
@@ -1,13 +1,11 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
 CREATE TABLE "cars_data" (
-    "ID" SERIAL   NOT NULL,
     "Brand" VARCHAR   NOT NULL,
     "Model" VARCHAR   NOT NULL,
     "Battery" float   NOT NULL,
-    "€/km_of_range" int   NOT NULL,
+    "km_of_range" int   NOT NULL,
     "0-100" float   NOT NULL,
     "Top_Speed" int   NOT NULL,
     "Range" int   NOT NULL,
@@ -16,11 +14,8 @@ CREATE TABLE "cars_data" (
     "Price_in_Germany_before_incentives" float   NOT NULL,
     "Price_in_The_Netherlands_before_incentives" float   NOT NULL,
     "Price_in_the_United_Kingdom_after_incentives" float   NOT NULL,
-    "Approx._in_USD_based_on_source" float   NOT NULL,
-    "Drive_Configuration" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_cars_data" PRIMARY KEY (
-        "ID"
-     )
+    "Approx_in_USD_based_on_source" float   NOT NULL,
+    "Drive_Configuration" VARCHAR   NOT NULL
 );
 
 SELECT * FROM public.cars_data
