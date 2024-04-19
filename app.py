a56d129792893b2985b1aefdc0b5a58ea65e4601
vanillatyy1 <vanilla.tyy@gmail.com>
2024-04-18 23:24:14 -0400

    fixing

---
+++ app.py
@@ -0,0 +1,39 @@
+# Import the dependencies.
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, text, func
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine("postgressql://postgres:1234@localhost:5432/cars_data_db")
+
+#Execute the query
+with engine.connect() as connection:
+    # Query all the data from the table
+    result = connection.execute(text('SELECT * FROM public.cars_data'))
+    
+    # Fetch all rows
+    rows = result.fetchall()
+
+    # Print the rows
+    for row in rows:
+        print(row)  
+
+# reflect an existing database into a new model 
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
---
+++ cars_data_schema.sql
@@ -0,0 +1,22 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE "cars_data" (
+    "Brand" VARCHAR,
+    "Model" VARCHAR,
+    "Battery" float,
+    "pound_per_km_of_range" VARCHAR,
+    "0-100" float,
+    "Top_Speed" int,
+    "Range" int,
+    "Efficiency" int,
+    "Fastcharge" float,
+    "Price_in_Germany_before_incentives" float,
+    "Price_in_The_Netherlands_before_incentives" float,
+    "Price_in_the_United_Kingdom_after_incentives" float,
+    "Approx_in_USD_based_on_soucre" float,
+    "Drive_Configuration" VARCHAR
+);
+
+SELECT * FROM public.cars_data
+LIMIT 100
