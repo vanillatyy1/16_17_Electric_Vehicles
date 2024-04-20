fa5df13c95620ac6e5cc9219b98eef0f0e148fa3
Amer Banaweer <amerbnr10@gmail.com>
2024-04-20 19:38:46 +0300

    coment

--- static/js/plot.js
+++ static/js/plot.js
@@ -123,40 +123,4 @@ function menuChange(selectedValue) {
     // You can perform any actions you want based on the selected value
 }
 
-// fetch('/battery_efficiency_vs_range')
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         // Parse the JSON string into a JavaScript object
-//         data = JSON.parse(data);
-
-//         // Extract Battery and Range data
-//         const batteryData = data.map(entry => entry.Battery);
-//         const rangeData = data.map(entry => entry.Range);
-
-//         // Create plot layout
-//         const batteryEfficiencyLayout = {
-//             title: 'Battery Efficiency vs Range',
-//             xaxis: { title: 'Battery Efficiency' },
-//             yaxis: { title: 'Range (km)' }
-//         };
-
-//         // Create plot data
-//         const batteryEfficiencyData = [{
-//             x: batteryData,
-//             y: rangeData,
-//             mode: 'markers',
-//             type: 'scatter'
-//         }];
-
-//         // Plot the data
-//         Plotly.newPlot('bar', batteryEfficiencyData, batteryEfficiencyLayout);
-//     })
-//     .catch(error => {
-//         console.error('Error fetching or processing data for Battery Efficiency vs Range:', error);
-//     });
-
+// Other two charts here
