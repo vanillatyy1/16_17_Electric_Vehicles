8a7a2ed22015dac5c6e0a6d6a95c82abc65e46c6
Amer Banaweer <amerbnr10@gmail.com>
2024-04-23 04:10:41 +0300

    update

--- app_01.py
+++ app_01.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
 from sqlalchemy import create_engine
+from config import endpoint, password
 
 # Create Flask app
 app = Flask(__name__)
 
 # Create engine
 # engine = create_engine("postgresql://postgres:Amer1234@localhost:5432/cars_data")
-engine = create_engine("postgresql://postgres:postgres1@{endpoint}:5432/cars_data")
+engine = create_engine(f"postgresql://postgres:{password}@{endpoint}:5432/cars_data")
 
 
 
